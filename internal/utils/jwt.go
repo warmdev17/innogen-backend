1bcb1b84dfe453ca82fe2f10a1be9637ff3e23f2
Hieu Le Trung <warmdevofficial@gmail.com>
2026-02-22 17:05:56 +0700

    feat(utils): add jwt token generation and parsing

---
+++ internal/utils/jwt.go
@@ -0,0 +1,27 @@
+// Package utils
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("maiphuongdangyeunhatvutru")
+
+func GenerateToken(userID uint, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     time.Now().Add(72 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func ParseToken(tokenStr string) (*jwt.Token, error) {
+	return jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+}
